40b989518ae41ad0d9c1c0e430f528d5bb2e0560
radumeditools <rdragan@meditools.nl>
2021-03-24 20:34:13 +0100

    sorter

--- extension.js
+++ extension.js
@@ -1,36 +1,51 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
+  console.log("function was init >> debug terminal");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sorter" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "sorter.dev",
+    function () {
+      const editor = vscode.window.activeTextEditor;
+      const selection = editor.selection;
+      function getEditorText() {
+        return editor.document.getText(editor.selection).split("\r\n");
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sorter.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      function sortLines(arrayLines) {
+        arrayLines = arrayLines.filter(function (e) {
+          return e;
+        });
+        return arrayLines.sort();
+      }
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from sorter!');
-	});
+      function rebuildToString(arrayLines) {
+        return arrayLines.join("\r\n");
+      }
 
-	context.subscriptions.push(disposable);
+      function addTextToDom(newText) {
+        editor.edit((builder) => builder.replace(selection, newText));
+      }
+
+      let text = undefined;
+      text = getEditorText();
+      text = sortLines(text);
+      text = rebuildToString(text);
+      console.log(text);
+
+      addTextToDom(text);
+
+      vscode.window.showInformationMessage("Hello World from sorter!");
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
